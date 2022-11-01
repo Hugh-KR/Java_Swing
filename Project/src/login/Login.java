7d3e367b59ca46837015949a753d2426ef04f246
YoungMoo Kim <rladuan612@gmail.com>
2022-11-01 14:55:32 +0900

    select
    조회 다이얼로그

--- Project/src/login/Login.java
+++ Project/src/login/Login.java
@@ -1,329 +1,102 @@
 package login;
 
-import java.awt.CardLayout;
-import java.awt.Component;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Insets;
+import java.awt.Label;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-public class Login {
+import loginDb.MemberDB;
 
-	JPanel cardPanel;
-	Login lp;
-	CardLayout card;
+public class Login extends JFrame {
+
+	public Login() {
+		JPanel p = new JPanel();
+		setLocationRelativeTo(p);
+		Label l1 = new Label("학사관리시스템", Label.CENTER);
+		Label l2 = new Label("ID");
+		l2.setAlignment(Label.CENTER);
+		Label l3 = new Label("PW");
+		l3.setAlignment(Label.CENTER);
+		getContentPane().add(l1);
+		getContentPane().add(l2);
+		getContentPane().add(l3);
+
+		TextField t1 = new TextField();
+		TextField t2 = new TextField();
+		getContentPane().add(t1);
+		getContentPane().add(t2);
+		t2.setEchoChar('*');
+
+		JButton j1 = new JButton("로그인");
+		JButton j2 = new JButton("회원가입");
+		JButton j3 = new JButton("ID/PW 찾기");
 	
+		getContentPane().add(j1);
+		getContentPane().add(j2);
+		getContentPane().add(j3);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Font font1 = new Font("굴림체", Font.BOLD, 30);
+		l1.setFont(font1);
 
-		Login lp = new Login();
-		lp.setFrame(lp);
-	}
+		l1.setBounds(100, 70, 300, 100);
+		l2.setBounds(110, 195, 40, 40);
+		l3.setBounds(110, 245, 40, 40);
 
-	public void setFrame(Login lpro) {
+		t1.setBounds(160, 200, 200, 30);
+		t2.setBounds(160, 250, 200, 30);
 
-		JFrame jf = new JFrame();
-		LoginPanel lp = new LoginPanel(lpro);
-		signupPanel sp = new signupPanel(lpro);
+		j1.setBounds(145, 330, 80, 30);
+		j2.setBounds(255, 330, 100, 30);
+		j3.setBounds(370, 250, 100, 30);
 
-		card = new CardLayout();
+		getContentPane().add(p);
+		setSize(500, 500);
+		setTitle("로그인");
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
 
-		cardPanel = new JPanel(card);
-		cardPanel.add(lp.mainPanel, "Login");
-		cardPanel.add(sp.mainPanel, "Register");
-		
-		jf.add(cardPanel);
-		jf.setVisible(true);
-		jf.setSize(500, 400);
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setLocationRelativeTo(null);
-		jf.setResizable(false);
-
-	}
-
-	public Connection getConnection() throws SQLException {
-		Connection conn = null;
-
-		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/member", "root",
-				"1234");
-
-		return conn;
-	}
-
-}
-
-class LoginPanel extends JPanel implements ActionListener {
-
-	JPanel mainPanel;
-	JTextField idTextField;
-	JPasswordField passTextField;
-
-	Login lp;
-	Font font = new Font("회원가입", Font.BOLD, 40);
-	
-
-	public LoginPanel(Login lp) {
-		this.lp = lp;
-		
-		mainPanel = new JPanel();
-		mainPanel.setLayout(new GridLayout(5, 1));
-		JPanel userPanel = new JPanel();
-		JPanel gridBagidInfo = new JPanel(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-
-		JLabel idLabel = new JLabel(" 아이디 : ");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
-		c.gridy = 0;
-		gridBagidInfo.add(idLabel, c);
-
-		idTextField = new JTextField(15);
-		c.insets = new Insets(0, 5, 0, 0);
-		c.gridx = 1;
-		c.gridy = 0;
-		gridBagidInfo.add(idTextField, c);
-
-		JLabel passLabel = new JLabel(" 비밀번호 : ");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
-		c.gridy = 1;
-		c.insets = new Insets(20, 0, 0, 0);
-		gridBagidInfo.add(passLabel, c);
-
-		passTextField = new JPasswordField(15);
-		c.insets = new Insets(20, 5, 0, 0);
-		c.gridx = 1;
-		c.gridy = 1;
-		gridBagidInfo.add(passTextField, c);
-
-		JPanel loginPanel = new JPanel();
-		JButton loginButton = new JButton("로그인");
-		loginPanel.add(loginButton);
-
-		JPanel signupPanel = new JPanel();
-		JButton signupButton = new JButton("회원가입");
-		loginPanel.add(signupButton);
-
-		mainPanel.add(userPanel);
-		mainPanel.add(gridBagidInfo);
-		mainPanel.add(loginPanel);
-		mainPanel.add(signupPanel);
-		
-		loginButton.addActionListener(this);
-
-		signupButton.addActionListener(new ActionListener() {
+		// 버튼에 대한 기능 구현(이벤트 처리)
+		j1.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				lp.card.next(lp.cardPanel);
+				String id = t1.getText();
+				String pwd = t2.getText();
+				MemberDB dao = new MemberDB();
+				int result = dao.login(id, pwd);
+				if (id.equals("manager")) {
+					JOptionPane.showMessageDialog(null, "관리자로 로그인 했습니다.");
+				} else if (result == 1) {
+					JOptionPane.showMessageDialog(null, " 로그인 했습니다.");
+				} else if (result == 0) {
+					JOptionPane.showMessageDialog(null, " 로그인이 실패했습니다.");
+				}
 			}
 		});
 
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		JButton jb = (JButton) e.getSource();
-
-		switch (e.getActionCommand()) {
-
-		case "로그인":
-
-			String id = idTextField.getText();
-			String pass = passTextField.getText();
-
-			try {
-
-				String sql_query = String.format("SELECT password FROM login WHERE id = '%s' AND password ='%s'",
-						id, pass);
-
-				Connection conn = lp.getConnection();
-				Statement stmt = conn.createStatement();
-
-				ResultSet rset = stmt.executeQuery(sql_query);
-				rset.next();
-
-				if (pass.equals(rset.getString(1))) {
-					JOptionPane.showMessageDialog(this, "Login Success", "로그인 성공", 1);
-				
-				} else
-					JOptionPane.showMessageDialog(this, "Login Failed", "로그인 실패", 1);
-
-			} catch (SQLException ex) {
-				JOptionPane.showMessageDialog(this, "Login Failed", "로그인 실패", 1);
-				System.out.println("SQLException" + ex);
-			}
-
-			break;
-
-		}
-	}
-
-} // class LoginPanel
-
-class signupPanel extends JPanel {
-
-	JTextField idTf;
-	JPasswordField passTf;
-	JPasswordField passReTf;
-	JTextField nameTf;
-	JTextField ageTf;
-	JPanel mainPanel;
-	JPanel subPanel;
-	JButton registerButton;
-	Font font = new Font("회원가입", Font.BOLD, 40);
-	String id = "", pass = "", passRe = "", name = "", age = "";
-	Login lp;
-
-	public signupPanel(Login lp) {
-
-		this.lp = lp;
-		subPanel = new JPanel();
-		subPanel.setLayout(new GridBagLayout());
-		subPanel.setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
-
-		JLabel idLabel = new JLabel("아이디 : ");
-		JLabel passLabel = new JLabel("비밀번호 : ");
-		JLabel passReLabel = new JLabel("비밀번호 재확인 : ");
-		JLabel nameLabel = new JLabel("이름 : ");
-		JLabel ageLabel = new JLabel("나이 : ");
-
-		idTf = new JTextField(15);
-		passTf = new JPasswordField(15);
-		passReTf = new JPasswordField(15);
-		nameTf = new JTextField(15);
-		ageTf = new JTextField(15);
-
-		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.insets = new Insets(15, 5, 0, 0);
-
-		c.gridx = 0;
-		c.gridy = 0;
-		subPanel.add(idLabel, c);
-
-		c.gridx = 1;
-		c.gridy = 0;
-		subPanel.add(idTf, c); // 아이디
-
-		c.gridx = 0;
-		c.gridy = 1;
-		subPanel.add(passLabel, c);
-
-		c.gridx = 1;
-		c.gridy = 1;
-		subPanel.add(passTf, c); // pass
-		
-		c.gridx = 2;
-		c.gridy = 1; 
-		subPanel.add(new JLabel("특수문자 + 8자"),c); //보안설정
-
-		c.gridx = 0;
-		c.gridy = 2;
-		subPanel.add(passReLabel, c);
-
-		c.gridx = 1;
-		c.gridy = 2;
-		subPanel.add(passReTf, c); // password 재확인
-
-		c.gridx = 0;
-		c.gridy = 3;
-		subPanel.add(nameLabel, c);
-
-		c.gridx = 1;
-		c.gridy = 3;
-		subPanel.add(nameTf, c); // 이름
-
-		c.gridx = 0;
-		c.gridy = 5;
-		subPanel.add(ageLabel, c);
-
-		c.gridx = 1;
-		c.gridy = 5;
-		subPanel.add(ageTf, c);
-
-		mainPanel = new JPanel();
-		mainPanel.setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
-		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-		JLabel signupLabel = new JLabel("회원가입");
-		signupLabel.setFont(font);
-		signupLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-		registerButton = new JButton("회원가입");
-		registerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-		mainPanel.add(signupLabel);
-		mainPanel.add(subPanel);
-		mainPanel.add(registerButton);
-
-
-		registerButton.addActionListener(new ActionListener() {      //회원가입버튼
+		// 회원가입 버튼을 누르면 회원가입하는 화면을 나오게 하고
+		j2.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				id = idTf.getText();
-				pass = new String(passTf.getPassword());
-				passRe = new String(passReTf.getPassword());
-				name = nameTf.getText();
-				age = ageTf.getText();
-
-				String sql = "insert into login(id, password, name, age) values (?,?,?,?)";
-
-				Pattern passPattern1 = Pattern.compile("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$"); //8자 영문+특문+숫자
-				Matcher passMatcher = passPattern1.matcher(pass);
-
-				if (!passMatcher.find()) {
-					JOptionPane.showMessageDialog(null, "비밀번호는 영문+특수문자+숫자 8자로 구성되어야 합니다", "비밀번호 오류", 1);
-				} else if (!pass.equals(passRe)) {
-					JOptionPane.showMessageDialog(null, "비밀번호가 서로 맞지 않습니다", "비밀번호 오류", 1);
-
-				} else {
-					try {
-						Connection conn = lp.getConnection();
-
-						PreparedStatement pstmt = conn.prepareStatement(sql);
-
-						pstmt.setString(1, idTf.getText());
-						pstmt.setString(2, pass);
-						pstmt.setString(3, nameTf.getText());
-						pstmt.setString(4, age);
-
-						int r = pstmt.executeUpdate();
-						System.out.println("변경된 row " + r);
-						JOptionPane.showMessageDialog(null, "회원 가입 완료!", "회원가입", 1);
-						lp.card.previous(lp.cardPanel); // 다 완료되면 로그인 화면으로
-					} catch (SQLException e1) {
-						System.out.println("SQL error" + e1.getMessage());
-						if (e1.getMessage().contains("PRIMARY")) {
-							JOptionPane.showMessageDialog(null, "아이디 중복!", "아이디 중복 오류", 1);
-						} else
-							JOptionPane.showMessageDialog(null, "정보를 제대로 입력해주세요!", "오류", 1);
-					} // try ,catch
-				}
+				JoinMember joinMember = new JoinMember();
+				dispose();
+			}
+		});
+		// IDPW찾기 구현중
+		j3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SearchMember searchMember = new SearchMember();
+				dispose();
 			}
 		});
 
---
+++ Project/src/login/StudentMain.java
@@ -0,0 +1,8 @@
+package login;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Login login = new Login();
+	}
+}
